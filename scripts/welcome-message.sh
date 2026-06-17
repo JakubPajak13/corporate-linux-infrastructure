335d0d1c6d08ff834365b6d128c25e13bde3b557
Jakub Pająk <jakub.pajak1304@gmail.com>
2026-06-17 14:29:13 +0200

    Update comments in welcome-message.sh

--- scripts/welcome-message.sh
+++ scripts/welcome-message.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-Global login banner for all PolMetal employees
-Deployed to /etc/profile.d/ to execute automatically upon successful login.
+#Global login banner for all PolMetal employees
+#Deployed to /etc/profile.d/ to execute automatically upon successful login.
 
 echo "Notice: This is a strictly monitored corporate server."
 echo "No work, no bonus."
